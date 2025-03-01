e7dbdc2f4515a9f4cf4c80453bfa8254f632331b
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-03-01 16:20:50 -0300

    <build>[String Initializr]: Criação da arquiterura spring e suas dependências com Spring Initializr

---
+++ src/main/kotlin/com/payroll/payrollSystem/PayrollSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.payroll.payrollSystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PayrollSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<PayrollSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/payroll/payrollSystem/PayrollSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.payroll.payrollSystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayrollSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
