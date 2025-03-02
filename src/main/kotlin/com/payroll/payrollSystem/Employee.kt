d54024c90c0f65404e08da71a08fc17744076673
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-03-02 15:48:55 -0300

    <feat>[Employee]: Modelagem da entidade empregado e sobre escrita de 'equals' e 'hashcode'

--- src/main/kotlin/com/payroll/payrollSystem/Employee.kt
+++ src/main/kotlin/com/payroll/payrollSystem/Employee.kt
@@ -1,4 +1,38 @@
 package com.payroll.payrollSystem
 
-class Employee {
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import org.apache.juli.logging.Log
+import java.util.Objects
+import kotlin.concurrent.timer
+
+@Entity
+data class Employee (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Long? = null,
+    private val name : String,
+    private var role : String
+) {
+
+    @Override
+    override fun equals(other: Any?): Boolean {
+
+        if (this === other){
+            return true
+        }
+        if (other !is Employee){
+            return false
+        }
+        other as Employee
+
+       return this.id == other.id && this.name == other.name && this.role == other.role
+    }
+
+    @Override
+    override fun hashCode(): Int {
+        return Objects.hash(this.id, this.name, this.role)
+    }
+
 }
